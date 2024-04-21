fbc8e1f6da570091be03ee55685f116e1cc9d248
mrcxmrj <marcin.zurawel@gmail.com>
2024-04-21 15:37:30 +0200

    read policy resource

--- main.go
+++ main.go
@@ -8,26 +8,40 @@ import (
 )
 
 type RolePolicy struct {
-	PolicyName     string `json:"PolicyName"`
-	PolicyDocument string `json:"PolicyDocument"`
+	PolicyName     string         `json:"PolicyName"`
+	PolicyDocument PolicyDocument `json:"PolicyDocument"`
 }
 
+// NOTE: edge case: statement can be singular instead of an array
 type PolicyDocument struct {
-	Version string
+	Version   string
+	Id        string
+	Statement []Statement
+}
+
+type Statement struct {
+	Sid          string
+	Effect       string
+	Principal    string
+	NotPrincipal string
+	Action       string
+	NotAction    string
+	Resource     string
+	NotResource  string
 }
 
 func main() {
-	rp := RolePolicy{"policydoc", "policyname"}
-	fmt.Println(rp)
-
-	jsonFile, err := os.Open("input/mock.json")
+	jsonFile, err := os.Open("input/test.json")
 	if err != nil {
 		fmt.Println(err)
 	}
 
 	byteValue, _ := io.ReadAll(jsonFile)
-	json.Unmarshal(byteValue, &rp)
+	var result RolePolicy
+	json.Unmarshal(byteValue, &result)
 
-	fmt.Println("opened json" + rp.PolicyName + rp.PolicyDocument)
+	for _, statement := range result.PolicyDocument.Statement {
+		fmt.Println("policy resource: " + statement.Resource)
+	}
 	defer jsonFile.Close()
 }
