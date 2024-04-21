cfa59ac7a24f99792eba3715d1f5c5f4715817de
mrcxmrj <marcin.zurawel@gmail.com>
2024-04-21 14:44:31 +0200

    add reading from json

--- main.go
+++ main.go
@@ -1,13 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"os"
+)
 
 type RolePolicy struct {
-	PolicyDocument string
-	PolicyName     string
+	PolicyName     string `json:"PolicyName"`
+	PolicyDocument string `json:"PolicyDocument"`
+}
+
+type PolicyDocument struct {
+	Version string
 }
 
 func main() {
 	rp := RolePolicy{"policydoc", "policyname"}
 	fmt.Println(rp)
+
+	jsonFile, err := os.Open("input/mock.json")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	byteValue, _ := io.ReadAll(jsonFile)
+	json.Unmarshal(byteValue, &rp)
+
+	fmt.Println("opened json" + rp.PolicyName + rp.PolicyDocument)
+	defer jsonFile.Close()
 }
